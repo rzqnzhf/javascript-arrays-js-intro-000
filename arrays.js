35072e1947378297929d9daaa197766bf2a70bb4
Skin <rizqinnazhif@gmail.com>
2019-11-20 09:48:09 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -39,6 +39,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  array.slice(array.length - 1);
+  array.slice(array.length);
   return array;
 }
