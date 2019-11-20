e0282669be531c244cd93ca93829a2f0e103f3e4
Skin <rizqinnazhif@gmail.com>
2019-11-20 09:24:36 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,35 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+var addElementToBeginningOfArray = (array, element) => {
+  var tambahAwal = [element, ...array];
+  return tambahAwal;
+};
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  var y = array.unshift(element);
+  return array;
+};
+
+function addElementToEndOfArray(array, element) {
+  var tambahBel = [...array, element];
+  return tambahBel;
+};
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  var x = array.push(element);
+  return array;
+};
+
+function accessElementInArray(array, index) {
+  return array[index];
+};
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  var x = array.pop();
+  return x;
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  var w = array.shift();
+  return w;
+}
