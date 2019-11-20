3ce4ddf12bf92ef7f2d539094d988a3264e80ba8
Skin <rizqinnazhif@gmail.com>
2019-11-20 09:57:47 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,43 +1,47 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 var addElementToBeginningOfArray = (array, element) => {
-  var tambahAwal = [element, ...array];
+  var tambahAwal = [element, ...array];// non-mutating
   return tambahAwal;
 };
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  var y = array.unshift(element);
+  var y = array.unshift(element);// mutate
   return array;
 };
 
 function addElementToEndOfArray(array, element) {
-  var tambahBel = [...array, element];
+  var tambahBel = [...array, element];// non-mutating
   return tambahBel;
 };
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  var x = array.push(element);
+  var x = array.push(element);// mutate
   return array;
 };
+
+////////////////////////////////////////////
 
 function accessElementInArray(array, index) {
   return array[index];
 };
 
+////////////////////////////////////////////
+
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  var c = array.shift();
+  var c = array.shift();// mutate
   return array;
 }
 
 function removeElementFromBeginningOfArray(array) {
-  return array.slice(1);
+  return array.slice(1);// non-mutating
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-  array.pop();
+  array.pop();// mutate
   return array;
 }
 
 function removeElementFromEndOfArray(array) {
-  return array.slice(0, array.length - 1);
+  return array.slice(0, array.length - 1);// non-mutating
 }
